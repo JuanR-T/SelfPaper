7609347bed4d662ed637d2397560c0ae85d48a2d
the-mastermind <the-mastermind@Ordinateur-portable-de-the-mastermind.local>
2024-06-29 02:00:57 +0200

    fixed gulp workflow to also work with tailwind inline css

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,28 +1,34 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 import colors from "tailwindcss/colors";
 import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
-/** @type {import('tailwindcss').Config} */
-export default {
+const config = {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    animation: {
+    extend: {
+      animation: {
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-    },
-    keyframes: {
+      },
+      keyframes: {
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
+    },
   },
-  plugins: [addVariablesForColors],
-}
+  plugins: [
+    tailwindcssAnimate,
+    addVariablesForColors,
+  ],
+} satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
@@ -34,3 +40,5 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
+
+export default config;
