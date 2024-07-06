fb7baf132dc7b990885a8e695a89302d545f701f
the-mastermind <the-mastermind@Ordinateur-portable-de-the-mastermind.local>
2024-07-07 01:06:36 +0200

    added burgerMenu according to window.innerwidth

--- frontend/src/components/ui/Contact.tsx
+++ frontend/src/components/ui/Contact.tsx
@@ -1,36 +1,40 @@
 import {
     LinkedinOutlined,
-    FacebookOutlined,
     SpotifyOutlined,
     TwitterOutlined,
     MailOutlined,
     InstagramOutlined,
 } from '@ant-design/icons';
+import clsx from 'clsx';
 
-const Contact = () => {
+const Contact = ({
+    isMobile
+}: {
+    isMobile: boolean
+}) => {
     return (
-        <div className="contact-section">
+        <div className={clsx("contact-section", isMobile ? "mobile" : "")}>
             <div className="socials">
                 <a target="blank" href="https://www.instagram.com/annechirol/">
-                    <InstagramOutlined />
+                    <InstagramOutlined /> {clsx(isMobile ? 'Instagram' : '')}
                 </a>
                 <a
                     target="blank"
                     href="https://open.spotify.com/show/7pppnKjXuYgYhJZAwhHXNE?si=7b8a5fe6c1734bcb"
                 >
-                    <SpotifyOutlined />
+                    <SpotifyOutlined /> {clsx(isMobile ? 'Spotify' : '')}
                 </a>
                 <a target="blank" href="https://twitter.com/AnneChirol16">
-                    <TwitterOutlined />
+                    <TwitterOutlined /> {clsx(isMobile ? 'X (Twitter)' : '')}
                 </a>
                 <a target="blank" href="mailto:anne.chirol@yahoo.fr">
-                    <MailOutlined />
+                    <MailOutlined /> {clsx(isMobile ? 'Mail' : '')}
                 </a>
                 <a
                     target="blank"
                     href="https://www.linkedin.com/in/anne-chirol-b0463810a/?originalSubdomain=fr"
                 >
-                    <LinkedinOutlined />
+                    <LinkedinOutlined /> {clsx(isMobile ? 'LinkedIn' : '')}
                 </a>
             </div>
         </div>
