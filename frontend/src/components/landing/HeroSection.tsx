d59b7d4fa6a667664ac5ecc72a1f82ff57cae811
the-mastermind <the-mastermind@Ordinateur-portable-de-the-mastermind.local>
2024-07-06 23:29:15 +0200

    WIP responsive frontpage scss

--- frontend/src/components/landing/HeroSection.tsx
+++ frontend/src/components/landing/HeroSection.tsx
@@ -29,10 +29,10 @@ const HeroSection = () => {
                 <div className="hero-content">
                     <h1>Anne Chirol</h1>
                     <div className="hero-biography">
-                        <span className="hero-flip-word">
+                        <span className="hero-subtitle">
                             Journaliste indépendante
                         </span>
-                        <a href="#biography-container" className="my-10 shadow-[0_0_0_1px_#000000_inset] px-6 py-2 bg-green-800 border border-white dark:border-black dark:text-black text-white rounded-xl font-bold transform hover:-translate-y-2 transition duration-200 hover:text-white">
+                        <a href="#biography-container" className="my-10 shadow-[0_0_0_1px_#000000_inset] px-6 py-2 bg-green-800 border border-white dark:border-black dark:text-black text-white rounded-xl font-semibold transform hover:-translate-y-2 transition duration-200 hover:text-white">
                             En savoir plus
                         </a>
                     </div>
--- frontend/src/components/ui/CustomCarousel.tsx
+++ frontend/src/components/ui/CustomCarousel.tsx
@@ -76,7 +76,7 @@ const CustomCarousel = () => {
                     <div className="carousel-description-box">
                         {currentPublicationData ? (
                             <>
-                                <h2 className='lg:text-2xl md:text-xl sm:text-sm font-bold dark:text-white text-center'>{currentPublicationData.title}</h2>
+                                <h2 className='lg:text-2xl md:text-xl sm:text-lg sm:text-center md:text-left text-black font-medium dark:text-white'>{currentPublicationData.title}</h2>
                                 <h3>
                                     <TagOutlined />{' '}
                                     {' ' + currentPublicationData.theme.title}
@@ -97,7 +97,7 @@ const CustomCarousel = () => {
                                     </span>
                                 </div>
                                 <a
-                                    className="my-10 w-fit shadow-[0_0_0_1px_#000000_inset] px-6 py-2 bg-green-800 border border-white dark:border-black dark:text-black text-white rounded-xl font-bold transform hover:-translate-y-2 transition duration-200 hover:text-white"
+                                    className="my-10 w-fit shadow-[0_0_0_1px_#000000_inset] px-6 py-2 bg-green-800 border border-white dark:border-black dark:text-black text-white rounded-xl font-semibold transform hover:-translate-y-2 transition duration-200 hover:text-white"
                                     target="_blank"
                                     href={currentPublicationData.link}
                                 >
@@ -111,7 +111,7 @@ const CustomCarousel = () => {
                     </div>
                 </div>
                 <div className="carousel-box" ref={carouselRef}>
-                    <LeftCircleOutlined onClick={prevCard} className='lg:text-4xl md:text-3xl sm:hidden carousel-button left' />
+                    <LeftCircleOutlined onClick={prevCard} className='lg:text-4xl md:text-3xl sm:text-2xl carousel-button left' />
                     {publicationsCarousel?.map((publicationCard) => {
                         return (
                             <div
